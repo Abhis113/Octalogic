7d09ad3a29a399ee023bdb799cc30d03fb6c9391
Abhis113 <abhishekranjangecd@gmail.com>
2023-09-13 18:29:59 +0530

    updated

---
+++ src/dashboard/app.tsx
@@ -0,0 +1,13 @@
+
+import { Router } from '../router';
+
+
+export function App() {
+  return (
+    <OctalogicAppProvider>
+      <Router />
+    </OctalogicAppProvider>
+  );
+}
+
+export default App;
